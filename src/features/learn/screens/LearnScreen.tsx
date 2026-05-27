8dbfd4377b6bc7e89ba571ada8beb6ff73acf1f2
Norton <norton_rayan@hotmail.com>
2026-05-26 23:18:08 -0300

    feat: creates navigation

---
+++ src/features/learn/screens/LearnScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function LearnScreen() {
+  return <View style={{flex:1,backgroundColor:"brown"}}/>;
+}
---
+++ src/features/settings/screens/SettingsScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from 'react-native';
+
+export function SettingsScreen() {
+  return <View style={{flex:1,backgroundColor:"black"}}/>;
+}

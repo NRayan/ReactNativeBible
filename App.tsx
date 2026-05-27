1298f24b152cc33ec43f355047ccf11c3f185c8b
Norton <norton_rayan@hotmail.com>
2026-05-27 18:48:32 -0300

    build: tailwind config

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Navigation } from './src/navigation';
+import "./global.css"
 
 function App() {
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content" />
       <Navigation />
+
+       <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+    </View>
     </SafeAreaProvider>
   );
 }
